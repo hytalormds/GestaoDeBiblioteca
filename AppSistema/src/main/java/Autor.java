d85ff7640e534c28efde99493b934aa0399d1fd5
Hytalo <hytalormds.contato@gmail.com>
2025-01-04 13:50:03 -0300

    Inserir e Excluir

--- AppSistema/src/main/java/Autor.java
+++ AppSistema/src/main/java/Autor.java
@@ -1,37 +1,72 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class Autor {
+
     private int ID_Autor;
     private String nome;
-    
-    public Autor(int ID_Autor, String nome){
+
+    public Autor(int ID_Autor, String nome) {
         this.ID_Autor = ID_Autor;
         this.nome = nome;
     }
-    
-    public int getIdAutor(){
+
+    public int getIdAutor() {
         return ID_Autor;
     }
-    
-    public void setIdAutor(int ID_Autor){
+
+    public void setIdAutor(int ID_Autor) {
         this.ID_Autor = ID_Autor;
     }
-    
-    public String getNome(){
+
+    public String getNome() {
         return nome;
     }
-    
-    public void setNome(String nome){
+
+    public void setNome(String nome) {
         this.nome = nome;
     }
-    
-    public void cadastrarAutor(String nome){
-        
+
+    public void cadastrarAutor(String nome) {
+        String insertQuery = "INSERT INTO Autor(nome) VALUES(?)";
+
+        try (Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement
+                = connection.prepareStatement(insertQuery)) {
+
+            preparedStatement.setString(1, nome);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Autor cadastrado!");
+            } else {
+                System.out.println("Autor não cadastrado!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
-    public void alterarAutor(String nome){
-        
+
+    public void alterarAutor(String nome) {
+
     }
-    
-    public void excluirUsuario(int ID_Autor){
-        
+
+    public void excluirAutor(int ID_Autor) {
+        String deleteQuery = "DELETE FROM Autor WHERE ID_Autor = ?";
+
+        try (Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement
+                = connection.prepareStatement(deleteQuery)) {
+            preparedStatement.setInt(1, ID_Autor);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Autor excluido");
+            } else {
+                System.out.println("Autor não excluido");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
