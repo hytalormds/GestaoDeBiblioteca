6956e55239125d380f705caafdf93ad4eb6543d5
Hytalo <hytalormds.contato@gmail.com>
2025-01-05 13:53:38 -0300

    Inserir e Excluir

--- AppSistema/src/main/java/Livro.java
+++ AppSistema/src/main/java/Livro.java
@@ -1,17 +1,20 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class Livro {
     private int ID_Livro;
-    private String titulo;
-    private int ano_publicacao;
-    private String status;
+    private String Titulo;
+    private int Ano_Publicacao;
+    private String Status;
     
     private Autor autor;
     private Editora editora;
     
-    public Livro(int ID_Livro, String titulo, int ano_publicacao, String status, Autor autor, Editora editora){
+    public Livro(int ID_Livro, String Titulo, int Ano_Publicacao, String Status, Autor autor, Editora editora){
         this.ID_Livro = ID_Livro;
-        this.titulo = titulo;
-        this.ano_publicacao = ano_publicacao;
-        this.status = status;
+        this.Titulo = Titulo;
+        this.Ano_Publicacao = Ano_Publicacao;
+        this.Status = Status;
         this.autor = autor;
         this.editora = editora;
     }
@@ -25,27 +28,27 @@ public class Livro {
     }
     
     public String getTitulo(){
-        return titulo;
+        return Titulo;
     }
     
-    public void setTitulo(String titulo){
-        this.titulo = titulo;
+    public void setTitulo(String Titulo){
+        this.Titulo = Titulo;
     }
     
     public int getAnoPublicacao(){
-        return ano_publicacao;
+        return Ano_Publicacao;
     }
     
-    public void setAnoPublicacao(int ano_publicacao){
-        this.ano_publicacao = ano_publicacao;
+    public void setAnoPublicacao(int Ano_Aublicacao){
+        this.Ano_Publicacao = Ano_Publicacao;
     }
     
     public String getStatus(){
-        return status;
+        return Status;
     }
     
-    public void setStatus(String status){
-        this.status = status;
+    public void setStatus(String Status){
+        this.Status = Status;
     }
     
     public Autor getAutor(){
@@ -57,16 +60,52 @@ public class Livro {
     }
     
     
-    public void cadastrarLivro(String titulo, int ano_publicacao, String status, Autor autor, Editora editora){
+    public void cadastrarLivro(String Titulo, int Ano_Publicacao, String Status, Autor autor, Editora editora){
+        String insertQuery = "INSERT INTO Livros(Titulo, Ano_Publicacao, Status, ID_Autor, ID_Editora) VALUES(?, ?, ?, ?, ?)";
         
+        try(Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement 
+                = connection.prepareStatement (insertQuery)){
+            
+            preparedStatement.setString(1, Titulo);
+            preparedStatement.setInt(2, Ano_Publicacao);
+            preparedStatement.setString(3, Status);
+            preparedStatement.setInt(4, autor.getIdAutor());
+            preparedStatement.setInt(5, editora.getIdEditora());
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            
+            if(rowsAffected > 0){
+                System.out.println("Livro cadastrado!");
+            }else{
+                System.out.println("Livro não excluido!");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
-    public void alterarLivro(String titulo, int ano_publicacao, String status, Autor autor, Editora editora){
+    public void alterarLivro(String Titulo, int Ano_Publicacao, String Status, Autor autor, Editora editora){
         
     }
     
     public void excluirLivro(int ID_Livro){
+        String deleteQuery = "DELETE FROM Livros WHERE ID_Livro = ?";
         
+        try(Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement 
+                = connection.prepareStatement(deleteQuery)){
+            
+            preparedStatement.setInt(1, ID_Livro);
+            
+            int rowsAffected = preparedStatement.executeUpdate();
+            
+            if(rowsAffected > 0){
+                System.out.println("Livro excluido!");
+            }else{
+                System.out.println("Livro não escluido!");
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
     
     public String pesquisarLivro(String titulo){
