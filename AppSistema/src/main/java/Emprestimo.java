1ae132f5c5445a0cb7748097880a31428040a5c1
Hytalo <hytalormds.contato@gmail.com>
2025-01-12 14:48:37 -0300

    Correção

--- AppSistema/src/main/java/Emprestimo.java
+++ AppSistema/src/main/java/Emprestimo.java
@@ -114,30 +114,50 @@ public class Emprestimo {
     }
 
     public void alterarEmprestimo(int ID_Emprestimo, Date Data_Emprestimo, Date Data_Devolucao, int Qtd_Livros, Usuario usuario, Livro livro) {
-        String updateQuery = "UPDATE Emprestimo SET Data_Emprestimo = ?, ";
+        String updateQuery = "UPDATE Emprestimo SET Data_Emprestimo = ?, Data_Devolucao = ?, Qtd_Livros = ?, ID_Usuario = ?, ID_Livro = ? WHERE ID_Emprestimo = ?";
+
+        try (Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement = connection.prepareStatement(updateQuery)) {
+
+            preparedStatement.setDate(1, new java.sql.Date(Data_Emprestimo.getTime()));
+            preparedStatement.setDate(2, new java.sql.Date(Data_Devolucao.getTime()));
+            preparedStatement.setInt(3, Qtd_Livros);
+            preparedStatement.setInt(4, usuario.getIdUsuario());
+            preparedStatement.setInt(5, livro.getIdLivro());
+            preparedStatement.setInt(6, ID_Emprestimo);  
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Empréstimo alterado!");
+            } else {
+                System.out.println("Empréstimo não atualizado!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public void excluirEmprestimo(int ID_Emprestimo) {
         String deleteQuery = "DELETE FROM Emprestimo WHERE Id_Emprestimo = ?";
 
         try (Connection connection = Conexao.getConexao(); PreparedStatement preparedStatement
-                = connection.prepareStatement(deleteQuery)){
-            
-         preparedStatement.setInt(1, ID_Emprestimo);
+                = connection.prepareStatement(deleteQuery)) {
 
-                int rowsAffected = preparedStatement.executeUpdate();
+            preparedStatement.setInt(1, ID_Emprestimo);
 
-                if (rowsAffected > 0) {
-                    System.out.println("Emprestimo excluido!");
-                } else {
-                    System.out.println("Emprestimo não excluido!");
-                }
-            }catch(Exception e){
-                e.printStackTrace();
-                }
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+                System.out.println("Emprestimo excluido!");
+            } else {
+                System.out.println("Emprestimo não excluido!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-        
-        /*public Date calcularEmprestimo(){
+    }
+
+    /*public Date calcularEmprestimo(){
        return 
    }*/
     public int pesquisarEmprestimo(int ID_Emprestimo) {
